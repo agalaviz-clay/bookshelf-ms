12a9734b65fbc6d2147f14be7c48b5cece7ab6bd
agalaviz-clay <167156051+agalaviz-clay@users.noreply.github.com>
2024-05-01 22:26:52 -0500

    Created SpringBoot framework files (#3)
    * Creating SpringBoot framework files
    
    * Resolved PR comments

---
+++ src/test/java/com/mtatgc/bookshelfms/BookshelfmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtatgc.bookshelfms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelfmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
