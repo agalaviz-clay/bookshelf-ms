4696150c3c1dbe34701e5c237d7e72769f3da4ea
agalaviz-clay <agalavizclay@gmail.com>
2024-09-05 13:57:14 -0500

    Remove wildcard imports

--- src/test/java/com/mtatgc/bookshelfms/controller/BookControllerTests.java
+++ src/test/java/com/mtatgc/bookshelfms/controller/BookControllerTests.java
@@ -22,7 +22,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BookController.class)
 public class BookControllerTests {
